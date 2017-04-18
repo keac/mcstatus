101deffa1b0c51df8a27b8a54b8190e66c0676b9
punchcode <waishelt9@gmail.com>
2017-04-18 11:16:06 +0800

    a bunch of TODOs
    haha

--- include/mcstatus/packet.hpp
+++ include/mcstatus/packet.hpp
@@ -25,6 +25,29 @@ protected:
     packet_t packets;
 }; // class packet
 
+class packet_builder
+{
+public:
+    packet_builder();
+    ~packet_builder();
+    void write_int8(int8_t v);
+    void write_uint8(uint8_t v);
+    void write_int16(int16_t v);
+    void write_uint16(uint16_t v);
+    void write_int32(int32_t v);
+    void write_int64(int64_t v);
+    void write_varint32(int32_t v);
+    void write_varint64(int64_t v);
+    void write_float(float v);
+    void write_double(double v);
+    void write_bool(bool v);
+    void write_string(std::string& v);
+    void clear();
+    packet_t completePacket(uint8_t packetID);
+
+  protected:
+    packet_t packet;
+};
 
 } // namespace mc
 
